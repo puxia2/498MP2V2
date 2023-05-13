f1dee593032c17f47b5fe806516ba97039c6108a
puxia2 <60232107+puxia2@users.noreply.github.com>
2023-05-12 21:21:48 -0400

    Add files via upload

---
+++ server.py
@@ -0,0 +1,21 @@
+from flask import Flask, request
+import subprocess
+import socket
+
+app = Flask(__name__)
+
+@app.route('/', methods=['POST', 'GET'])
+def server():
+    if request.method == "POST":
+        # Create a separate process to run stress_cpu.py
+        subprocess.Popen(["python3", "stress_cpu.py"])
+
+    elif request.method == "GET":
+        # Return the private IP address of the EC2 instance
+        ip_address = socket.gethostbyname(socket.gethostname())
+        return ip_address
+
+    return "Request processed"
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
